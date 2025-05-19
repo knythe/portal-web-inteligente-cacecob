8f53db2bf3576bace0c7ea9a70167cdfe883fe38
Knythe <krondoyv@ucvvirtual.edu.pe>
2025-05-19 18:21:13 -0500

    portal-web templates

---
+++ resources/views/template-plantilla.blade.php
@@ -0,0 +1,17 @@
+<!--LLamas a las normativas de la plantilla template-->
+@extends('template')
+
+@section('title','Home')
+
+@push('css')
+
+@endpush
+
+
+@section('content')
+<!--Contenido-->
+@endsection
+
+@push('js')
+
+@endpush
---
+++ resources/views/template.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/png" href="../assets/img/icon.png">
+    <title> @yield('title')</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="../assets/css/tailwind.output.css" />
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <script src="../assets/js/init-alpine.js"></script>
+    @stack('css')
+</head>
+
+<body>
+    <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen}">
+        <!-- Desktop sidebar -->
+        <!-- menu-bar-sidebar-->
+        <x-navigation-menu></x-navigation-menu>
+        <!-- menu-bar-sidebar-->
+        <!-- Mobile sidebar -->
+        <!-- Backdrop -->
+        <div x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
+        <!-- menu-bar-sidebar-->
+        <x-navigation-menu-movil></x-navigation-menu-movil>
+        <!-- menu-bar-sidebar-->
+        <div class="flex flex-col flex-1">
+            <!-- header -->
+            <x-navigation-header></x-navigation-header>
+            <!-- header -->
+            <main class="h-full pb-16 overflow-y-auto">
+                <!-- Remove everything INSIDE this div to a really blank page -->
+                <div class="container px-6 mx-auto grid">
+                    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+                        @yield('content')
+                    </h2>
+                </div>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
